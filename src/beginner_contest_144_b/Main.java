869ccd49cce10fbdcac16c0a45ceaa81238ade72
NITA-san <ysnitta@gmail.com>
2019-10-31 22:58:27 +0900

    144-Bを解く

---
+++ src/beginner_contest_144_b/Main.java
@@ -0,0 +1,22 @@
+package beginner_contest_144_b;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+
+		// 出力
+		for (int i = 1; i <= 9; i++) {
+			if (a % i == 0 && a / i >= 1 && a / i <= 9) {
+				System.out.println("Yes");
+				return;
+			}
+		}
+		System.out.println("No");
+
+	}
+
+}
