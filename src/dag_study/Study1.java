2ce93b5a0fa1c71987db053ae499201be47c0af3
NITA-san <ysnitta@gmail.com>
2019-11-18 23:56:11 +0900

    DAGの勉強

---
+++ src/dag_study/Study1.java
@@ -0,0 +1,22 @@
+package dag_study;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Study1 {
+
+	public static void main(String[] args) {
+
+		List<List<Integer>> graph = new ArrayList<List<Integer>>();
+		
+		Integer[] array = {1,2};
+		graph.add(Arrays.asList(array));
+		
+		
+		
+//		List<List<Integer>> graph = new ArrayList<Integer>();
+		
+	}
+
+}
